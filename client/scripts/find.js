9745fb6be492c9935480fc9f063a8830b85b689a
borecjeborec1 <robingrundel@seznam.cz>
2022-02-15 15:11:02 +0100

    fixed lowecase

--- client/scripts/find.js
+++ client/scripts/find.js
@@ -2,7 +2,7 @@
 function findData(str) {
   let res = data
   for (let i = 0; i < res.length; i++) {
-    if (res[i][0].includes(str)) {
+    if (res[i][0].includes(str.toLowerCase())) {
       return { question: res[i][0], answer: res[i][1] }
     }
   }
