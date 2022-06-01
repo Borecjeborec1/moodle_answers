1335cc774dea823e5279a2b7db861da233c0be09
borecjeborec1 <robingrundel@seznam.cz>
2022-06-01 22:35:47 +0200

    debugging

--- client/scripts/find.js
+++ client/scripts/find.js
@@ -2,6 +2,7 @@
 function findData(str) {
   let res = data
   for (let i = 0; i < res.length; i++) {
+    console.log(res[i][0], str.trim().toLowerCase().replace(/ /g, ""))
     if (res[i][0].includes(str.trim().toLowerCase().replace(/ /g, ""))) {
       return { question: res[i][0], answer: res[i][1] }
     }
